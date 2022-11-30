02c4de8d2f270e0ff57e5d47ed81a1189f018e6f
Neri Espinola <neri.agustin.es@outlook.com>
2022-11-30 12:37:35 -0300

    feat: se crea el Modulo Administracion de Usuarios
    * Se trasladan los componentes de Usuarios al nuevo modulo

--- src/app/administracion-usuarios/components/form/form.component.ts
+++ src/app/administracion-usuarios/components/form/form.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { UsuarioService } from '../../services/usuario.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
-  selector: 'app-afiliado-form',
-  templateUrl: './afiliado-form.component.html',
-  styleUrls: ['./afiliado-form.component.css']
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
 })
-export class AfiliadoFormComponent {
-
+export class FormComponent {
   constructor(private usuarioService:UsuarioService) { }
 
   onClickSubmit(formUsuario: NgForm) {
@@ -24,10 +23,9 @@ export class AfiliadoFormComponent {
 
       formUsuario.reset();
     } else {
-      
+
     }
 
     return false;
   }
-
 }
